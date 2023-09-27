bc0e0b49716453b4e3d66d2b7f0475d179305315
tanexc <artur.231456@gmail.com>
2023-09-28 00:10:08 +0300

    auth screen edit. sign up, sign in, auth as guest

---
+++ app/src/main/java/ru/tanexc/notegraph/presentation/screen/auth/WelocmeScreen.kt
@@ -0,0 +1,111 @@
+package ru.tanexc.notegraph.presentation.screen.auth
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Login
+import androidx.compose.material.icons.filled.NoAccounts
+import androidx.compose.material.icons.outlined.HowToReg
+import androidx.compose.material.icons.outlined.Login
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedIconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import ru.tanexc.notegraph.R
+
+@Composable
+fun WelocmeScreen(
+    colorScheme: ColorScheme,
+    onSignUp: () -> Unit,
+    onSignIn: () -> Unit,
+    onAuthAsGuest: () -> Unit
+) {
+
+    Box(Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.7f)
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            FilledIconButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { onSignIn() }
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Outlined.Person, null)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(stringResource(R.string.enter))
+                }
+            }
+
+
+            FilledIconButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { onSignUp() }
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Outlined.HowToReg, null)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(stringResource(R.string.sign_up))
+                }
+            }
+
+            Spacer(modifier = Modifier.size(16.dp))
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Spacer(
+                    modifier = Modifier
+                        .height(1.dp)
+                        .background(colorScheme.outline)
+                        .weight(0.5f)
+                )
+                Spacer(modifier = Modifier.size(8.dp))
+                Text(stringResource(R.string.or))
+                Spacer(modifier = Modifier.size(8.dp))
+                Spacer(
+                    modifier = Modifier
+                        .height(1.dp)
+                        .background(colorScheme.outline)
+                        .weight(0.5f)
+                )
+            }
+
+            Spacer(modifier = Modifier.size(16.dp))
+
+            OutlinedIconButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { onSignUp() }
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Filled.NoAccounts, null)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(stringResource(R.string.auth_as_guest))
+                }
+            }
+
+
+        }
+    }
+
+}
