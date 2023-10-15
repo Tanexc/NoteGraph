80e1d2ac5a401d29880461d530107044975ca88b
tanexc <artur.231456@gmail.com>
2023-10-15 23:23:01 +0300

    DraggableComponent fix

--- app/src/main/java/ru/tanexc/notegraph/presentation/ui/widgets/note/DraggableComponent.kt
+++ app/src/main/java/ru/tanexc/notegraph/presentation/ui/widgets/note/DraggableComponent.kt
@@ -1,18 +1,15 @@
 package ru.tanexc.notegraph.presentation.ui.widgets.note
 
-import android.util.Log
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
-import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,20 +23,18 @@ import kotlin.math.roundToInt
 fun DraggableComponent(
     startOffset: IntOffset,
     enabled: Boolean = true,
-    enabledIndication: Modifier.() ->Modifier,
     onOffsetChange: ((IntOffset) -> Unit)? = null,
-    onRelease: () -> Unit,
-    content: @Composable () -> Unit
+    onRelease: (IntOffset) -> Unit,
+    content: @Composable (BoxScope.(Boolean) -> Unit)
 ) {
     var offset: IntOffset by remember { mutableStateOf(startOffset) }
-    Box(
-        content = { content() },
+    Column(
         modifier =
         if (enabled) Modifier
             .offset { offset }
             .pointerInput(Unit) {
                 detectDragGestures(
-                    onDragEnd = onRelease
+                    onDragEnd = { onRelease(offset) }
                 ) { change, dragAmount ->
                     change.consume()
                     val offsetChange =
@@ -50,7 +45,10 @@ fun DraggableComponent(
                     }
                 }
             }
-            .enabledIndication()
         else Modifier.offset { offset }
-    )
+    ) {
+        Box(
+            content = { content(enabled) }
+        )
+    }
 }
--- app/src/main/java/ru/tanexc/notegraph/presentation/ui/widgets/note/TextPieceComponent.kt
+++ app/src/main/java/ru/tanexc/notegraph/presentation/ui/widgets/note/TextPieceComponent.kt
@@ -5,17 +5,14 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ColorScheme
@@ -27,7 +24,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import com.google.firebase.annotations.concurrent.Background
 import ru.tanexc.notegraph.domain.model.note.TextPiece
 import ru.tanexc.notegraph.presentation.ui.theme.Typography
 
