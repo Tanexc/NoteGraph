2647b686d27f8da94c1e807f83cc8d5ff12d424f
tanexc <artur.231456@gmail.com>
2023-11-08 22:43:12 +0300

    preference card edit

--- app/src/main/java/ru/tanexc/notegraph/presentation/ui/shapes/PreferencesItemShape.kt
+++ app/src/main/java/ru/tanexc/notegraph/presentation/ui/shapes/PreferencesItemShape.kt
@@ -1,12 +1,11 @@
 package ru.tanexc.notegraph.presentation.ui.shapes
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 
-fun Modifier.firstPreferenceShape(): Modifier = this.clip(RoundedCornerShape(22.dp, 22.dp, 4.dp, 4.dp))
+fun firstPreferenceShape(): Shape = RoundedCornerShape(22.dp, 22.dp, 4.dp, 4.dp)
 
-fun Modifier.lastPreferenceShape(): Modifier = this.clip(RoundedCornerShape(4.dp, 4.dp, 22.dp, 22.dp))
+fun lastPreferenceShape(): Shape = RoundedCornerShape(4.dp, 4.dp, 22.dp, 22.dp)
 
-fun Modifier.middlePreferenceShape(): Modifier = this.clip(RoundedCornerShape(4.dp))
+fun middlePreferenceShape(): Shape = RoundedCornerShape(4.dp)
--- app/src/main/java/ru/tanexc/notegraph/presentation/ui/widgets/cards/PreferenceCard.kt
+++ app/src/main/java/ru/tanexc/notegraph/presentation/ui/widgets/cards/PreferenceCard.kt
@@ -3,6 +3,7 @@ package ru.tanexc.notegraph.presentation.ui.widgets.cards
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -13,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
-import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -22,20 +22,36 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun PreferenceCard(
     modifier: Modifier = Modifier,
+    borderEnabled: Boolean,
+    borderColor: Color = Color.Transparent,
+    shape: Shape,
     header: @Composable RowScope.() -> Unit,
     content: @Composable ColumnScope.() -> Unit
 ) {
     var collapsed: Boolean by remember { mutableStateOf(true) }
 
-    Column(modifier = modifier.animateContentSize()) {
+    Column(
+        modifier = Modifier
+            .clip(shape)
+            .border(1.dp, borderColor.copy(if (borderEnabled) 0.7f else 0f), shape)
+            .then(modifier)
+            .animateContentSize()
+    ) {
 
-        Row(modifier = modifier.clickable { collapsed = !collapsed }.padding(22.dp)) {
+        Row(modifier = Modifier
+            .clip(shape)
+            .clickable { collapsed = !collapsed }
+            .padding(22.dp)
+        ) {
             header()
             Spacer(modifier = Modifier.size(22.dp))
             IconButton(
